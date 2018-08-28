64fd48dd7b384d167385d976a84517dda058f366
AlanKuoFromTaiwan <42194216+AlanKuoFromTaiwan@users.noreply.github.com>
2018-08-28 16:17:05 +0800

    Adding facts array

--- app/src/main/java/com/alan/funfactsalan/FunFactsAlanActivity.java
+++ app/src/main/java/com/alan/funfactsalan/FunFactsAlanActivity.java
@@ -1,13 +1,48 @@
 package com.alan.funfactsalan;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class FunFactsAlanActivity extends AppCompatActivity {
+    //Declare our view variables
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts_alan);
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
+                // The button was clicked, so update the factTextView with new facts
+                // Update the screen with our new fact
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(3);
+                String fact = randomNumber + "";
+                factTextView.setText(fact);
+            }
+
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
