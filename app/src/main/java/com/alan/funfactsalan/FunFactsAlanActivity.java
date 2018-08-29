ef214ab9cce6e3e5545558e983bde0c9074e7e8c
AlanKuoFromTaiwan <42194216+AlanKuoFromTaiwan@users.noreply.github.com>
2018-08-29 09:22:29 +0800

    refactored

--- app/src/main/java/com/alan/funfactsalan/FunFactsAlanActivity.java
+++ app/src/main/java/com/alan/funfactsalan/FunFactsAlanActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class FunFactsAlanActivity extends AppCompatActivity {
+    private FactBook factBook = new FactBook();
     //Declare our view variables
     private TextView factTextView;
     private Button showFactButton;
@@ -23,23 +24,9 @@ public class FunFactsAlanActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-                // The button was clicked, so update the factTextView with new facts
+
                 // Update the screen with our new fact
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                String fact = randomNumber + "";
-                fact = facts[randomNumber];
+                String fact = factBook.getFact();
                 factTextView.setText(fact);
             }
 
